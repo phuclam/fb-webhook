610af5d90b86dfb53eda2c12fb1bfd330bf97e70
phuclam <mrjkey89@gmail.com>
2019-12-03 20:40:44 +0700

    Update

---
+++ socket.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const
+    bodyParser = require('body-parser'),
+    config = require('config'),
+    crypto = require('crypto'),
+    express = require('express'),
+    https = require('https'),
+    request = require('request');
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({verify: verifyRequestSignature}));
+app.use(express.static('public'));
+
+//Socket.io
+var server = app.listen(5000, function(){
+    console.log('listening for requests on port 5000,');
+});
+const io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+io.on('sendMessage', function (senderID, messageText) {
+    //sendTextMessage(senderID, messageText);
+});
+
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+
+    if (!signature) {
+        // For testing, let's log an error. In production, you should throw an
+        // error.
+        console.error("Couldn't validate the signature.");
+    } else {
+        var elements = signature.split('=');
+        var method = elements[0];
+        var signatureHash = elements[1];
+
+        var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
+}
