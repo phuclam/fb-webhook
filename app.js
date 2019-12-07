4c7235224dcf17418e38b194a8c9976598d7e4af
phuclam <mrjkey89@gmail.com>
2019-12-07 14:54:27 +0700

    Update

--- app.js
+++ app.js
@@ -23,12 +23,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json({verify: verifyRequestSignature}));
 app.use(express.static('public'));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    /*res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");*/
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Set-Cookie", "Secure;SameSite=None");
     next();
 });
 //Socket.io
