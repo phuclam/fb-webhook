a4cc37a0577ddd2062c4b7fd6484a5ffa61ab2f9
phuclam <mrjkey89@gmail.com>
2019-12-07 14:50:20 +0700

    Update

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Set-Cookie", "Secure;SameSite=None");
     next();
 });
 //Socket.io
