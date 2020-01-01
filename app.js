a86268cdaccffad5c79e6a90ad5b1436ae7cb23c
phuclam <mrjkey89@gmail.com>
2020-01-01 21:22:20 +0700

    Update

--- app.js
+++ app.js
@@ -68,7 +68,12 @@ var Message = mongoose.model('messages', messageSchema);
 var Recipient = mongoose.model('recipients', recipientSchema);
 
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, function (err) {
     if (err) {
         throw err;
     }
